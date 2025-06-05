42572feb32533f98e00294116d83127f433c7a7c
SidApp77 <dspmaspsudip@gmail.com>
2025-06-05 12:07:32 +0530

    test

--- resources/views/admin/payment/list.blade.php
+++ resources/views/admin/payment/list.blade.php
@@ -5,7 +5,7 @@
 @section('main')
 <!-- Main Content Area -->
 <main class="flex-1 overflow-auto bg-gray-50 flex items-center justify-center text-gray-600">
-    <h1 class="text-xl font-semibold">Payment list</h1>
+    <h1 class="text-xl font-semibold">Payment list updated </h1>
 </main>
 
 @endsection
